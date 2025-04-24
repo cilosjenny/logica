bc2553ac0fa44743640042216dbf3e37f0b68206
jenny <jenniferbarbosa4439@gmail.com>
2025-04-24 17:11:03 -0300

    script

---
+++ tentativa.js
@@ -0,0 +1,16 @@
+const readline = require ("readline-sync")
+const numeros = []
+
+
+for (let i=1; i <=3;i++){
+    let numero=Number(readline.question('informe o numero' +i ))
+     numeros.push(numero)
+
+}
+//multiplicando por ele mesmo agr
+for (let i=0;i<numeros.length;i++){
+    let numero =numeros[1]*numeros[1]
+    resultado.push(numero)
+    const resultado = []
+    console.log(resultado)
+}
