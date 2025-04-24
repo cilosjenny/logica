9f68d0cc713d3288ad7b4f41be0157e56e5d2edd
cilosjenny <geracaotech@digitalcollege.internal>
2025-04-24 15:09:09 -0300

    console

--- imc.js
+++ imc.js
@@ -1,23 +1,29 @@
-const readline = require ("require-sync");
-
+const readline = require ("readline-sync");
+//para chamar a biblioteca 
 let peso = parseInt (readline.question('Qual seu peso?'))
-let altura = parseInt (readline.question('Qual sua altura?'))
+//ou number ( ) 
+//parsefloat=se o núm tiver ponto ex:1.65
+let altura = parseFloat(readline.question('Qual sua altura?'))
+let imc = peso/(altura*altura)
+//calculo para dividir
+console.log ("seu imc é" +imc)
+
 
 if (imc <18.5){
-    console.log (abaixo do peso)
+    console.log ("abaixo do peso")
 }
-else if (imc >=18.5 && <=24,9){
-    console.log (normal)
+else if (imc >=18.5 && imc <=24.9){
+    console.log ("normal")
 
 }
-else if (imc <=25 && <30){
-    console.log (acima do peso)
+else if (imc <=25 && imc <30){
+    console.log ("acima do peso")
 
 }
-else (imc >=40) {
-    console.log (peso morbido) 
+else {
+    console.log ("peso morbido")
 }
 
-
+    
 
 
